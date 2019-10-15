1b7f03098118bda31578b0da9d1ead46d90a219b
Husin <Muhamad.husin0412@gmail.com>
2019-10-16 01:43:58 +0700

    tertinggal
    ok

--- public/admin/assets/scripts/posting-home.js
+++ public/admin/assets/scripts/posting-home.js
@@ -1,7 +1,7 @@
 
             // POSTING BERITA
             
-            var docRef = firebase.firestore().collection("Posting");
+         var docRef = firebase.firestore().collection("Posting");
 			var firestore = firebase.firestore();
 			var storage = firebase.storage();
 			var storageRef = storage.ref();
@@ -24,7 +24,7 @@
 				let hasil = id.toString();
 
 				let new_html = '';
-				new_html = '<div class="col-lg-3 col-md-6 single-blog"><div class="thumb"><img class="img-fluid"  src="'+data.image+'" alt=""></div><p class="meta">'+data.tanggal+' Dari | <a href="#"> '+data.dari+'</a></p><a href="#"><h5>'+data.judul+'</h5></a><p>'+data.deskripsi+'.</p><a href="#" class="details-btn d-flex justify-content-center align-items-center"><span class="details">Details</span><span class="lnr lnr-arrow-right"></span></a></div>'; 
+				new_html = '<div class="col-lg-3 col-md-6 single-blog"><div class="thumb"><img class="img-fluid"  src="'+data.image+'" alt=""></div><p class="meta">'+data.tanggal+' Dari | <a href="#"> '+data.dari+'</a></p><a href="#post"><h5>'+data.judul+'</h5></a><p>'+data.deskripsi+'.</p><a href="#post" class="details-btn d-flex justify-content-center align-items-center"><span class="details">Details</span><span class="lnr lnr-arrow-right"></span></a></div>'; 
             	$('#post').find('#postItem').append(new_html);
 			}
 
